814ba4a291de864b6ecaf189ca32207f388b1f68
tonyandhika20 <tonyandhika20@gmail.com>
2020-10-03 22:13:32 +0700

    Tugas RecyclerView DTS 2020

---
+++ app/src/main/java/id/primadev/recyclerview/MainActivity.java
@@ -0,0 +1,64 @@
+package id.primadev.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class MainActivity extends AppCompatActivity implements ContactAdapter.OnContactClickListener {
+
+    public RecyclerView rv;
+    public ContactAdapter contactAdapter;
+    public RecyclerView.LayoutManager layoutManager;
+    public List<Contact> listContact = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rv = findViewById(R.id.rvContact);
+        //List kontak yang akan di tampilkan (nama, telepon, url gambar)
+        daftarContact();
+
+
+        contactAdapter = new ContactAdapter(listContact);
+        contactAdapter.setListener(this);
+        layoutManager = new LinearLayoutManager(getApplicationContext());
+        rv.setAdapter(contactAdapter);
+        rv.setLayoutManager(layoutManager);
+
+    }
+
+    @Override
+    public void onClick(View view, int position) {
+        Contact contact = listContact.get(position);
+        Toast.makeText(this, "Hallo "+contact.getName(), Toast.LENGTH_LONG)
+                .show();
+    }
+
+    private void daftarContact(){
+        listContact.add(new Contact("Iron Man",
+                "102018308",
+                "https://cdn4.iconfinder.com/data/icons/famous-characters-add-on-vol-1-flat/48/Famous_Character_-_Add_On_1-14-512.png"));
+
+        listContact.add(new Contact("Bat Man",
+                "102018309",
+                "https://cdn4.iconfinder.com/data/icons/famous-characters-add-on-vol-1-flat/48/Famous_Character_-_Add_On_1-22-512.png"));
+
+        listContact.add(new Contact("Groot",
+                "102018307",
+                "https://cdn4.iconfinder.com/data/icons/famous-characters-add-on-vol-1-flat/48/Famous_Character_-_Add_On_1-21-512.png"));
+
+        listContact.add(new Contact("Sonic",
+                "102018301",
+                "https://cdn4.iconfinder.com/data/icons/famous-characters-add-on-vol-1-flat/48/Famous_Character_-_Add_On_1-26-512.png"));
+    }
+}
